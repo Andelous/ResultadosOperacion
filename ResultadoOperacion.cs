abafdefba41e2a703437c46dbb131674ff1646da
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2017-05-08 02:46:05 -0500

    Namespace cambiado.

--- ResultadoOperacion.cs
+++ ResultadoOperacion.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DepartamentoServiciosEscolaresCBTis123.Logica.Utilerias
+namespace ResultadosOperacion
 {
     /// <summary>
     /// Representa el resultado de una operación llevada a cabo por la aplicación.
